637353a966c3c47544818cc7ccd5c805a95d7fdb
Joe <joe7663@gmail.com>
2023-03-07 16:37:23 -0500

    endpoint work 2

--- project-template-h2-main/src/main/java/Controller/GroceryController.java
+++ project-template-h2-main/src/main/java/Controller/GroceryController.java
@@ -1,9 +1,11 @@
 package Controller;
 
-import Model.Grocery;
+import Model.Product;
 import Model.User;
-import Service.GroceryService;
+import Model.Cart;
+import Service.ProductService;
 import Service.UserService;
+import Service.CartService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.JsonpCharacterEscapes;
 import com.fasterxml.jackson.core.io.JsonEOFException;
@@ -13,33 +15,33 @@ import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 public class GroceryController{
-    GroceryService groceryService;
+    ProductService productService;
     UserService userService;
     public GroceryController(){
-        this.groceryService = new GroceryService();
+        this.productService = new ProductService();
         this.userService = new UserService;
     }
 
     public Javalin startAPI(){
         Javalin app = Javalin.create();
 
-        app.post("/register", this::postUserHandeler);
+        app.post("/register", this::postUserHandler);
         app.post("/login", this::postLoginUserHandler);
         //app.patch("/member/{member_id}", this::patchUserInfoHandler);
         app.patch("/cart/{quantity}", this:patchCartHandler());
         app.get("/cart", this::getCartHandler);
-        app.get("/cart/checkout", this::getCheckoutMemberHandler);
-        app.post("/cart/checkout", this::postCheckoutHandler);
-        app.get("/grocery", this::getGroceriesHandler);
-        app.get("/grocery/{item}", this::getItemHandler);
-        app.get("/grocery/{search}", this::getSearchHandler);
-        app.get("/grocery/{filters}", this::getFiltersHandler);
+        app.get("/checkout/{member_id}", this::getCheckoutMemberHandler);
+        //app.post("/cart/checkout", this::postCheckoutHandler);
+        app.get("/product", this::getProductsHandler);
+        app.get("/product/{item}", this::getItemByNameHandler);
+        app.get("/product/{search}", this::getSearchHandler);
+        app.get("/product/{filters}", this::getFiltersHandler);
         return app;
     }
 
-    private void postUserHandeler(Context ctx) throws JsonProcessingException{
+    private void postUserHandler(Context ctx) throws JsonProcessingException{
         ObjectMapper om = new ObjectMapper();
-        User user = om.readVaoue(ctx.body(), User.class);
+        User user = om.readValue(ctx.body(), User.class);
         User newUser = userService.addUser(user);
         if(newUser != null){
             ctx.json(newUser);
@@ -65,45 +67,47 @@ public class GroceryController{
 
     private void patchCartHandler(Context ctx) throws JsonProcessingException{
         ObjectMapper om = new ObjectMapper();
-        Grocery grocery = om.readValue(ctx.body(), Grocery.class);
-        int upc = grocery.getUpc();
+        Cart cart = om.readValue(ctx.body(), Cart.class);
+        int upc = product.getUpc();
         String q_input = ctx.pathParam("quantity");
         int q = Integer.parseInt(q_input);
-        Grocery patchedGrocery = GroceryController.patchCartByUpc(upc, q);
+        Cart patchedCart = CartService.patchCartByUpc(upc, q);
     }
 
     private void getCartHandler(Context ctx) throws JsonProcessingException{
-        ctx.json(groceryService.getCart());
+        ctx.json(cartService.getCart());
     }
 
     private void getCheckoutMemberHandler(Context ctx) throws JsonProcessingException{
-
+        String user_id = ctx.pathParam("member_id");
+        int id = Integer.parseInt(user_id);
+        ctx.json(userService.getCredentials(id));
     }
 
-    private void postCheckoutHandler(Context ctx) throws JsonProcessingException{
+    /*private void postCheckoutHandler(Context ctx) throws JsonProcessingException{
 
+    }*/
+
+    private void getProductsHandler(Context ctx) throws JsonProcessingException{
+        ctx.json(productService.getAllGroceries());
     }
 
-    private void getGroceriesHandler(Context ctx) throws JsonProcessingException{
-        ctx.json(groceryService.getAllGroceries());
-    }
-
-    private void getItemHandler(Context ctx) throws JsonProcessingException{
+    private void getItemByNameHandler(Context ctx) throws JsonProcessingException{
         String item_input = ctx.pathParam("item");
-        Grocery grocery = groceryService.getGroceryByName(item_input);
-        if(grocery != null){
-            ctx.json(grocery);
+        Product product = productService.getProductByName(item_input);
+        if(product != null){
+            ctx.json(product);
         }
     }
     private void getSearchHandler(Context ctx) throws JsonProcessingException{
         String search_input = ctx.pathParam("search");
-        Grocery grocery = groceryService.getGroceryByName(search_input);
-        if(grocery != null){
-            ctx.json(grocery);
+        Product product = productService.getProductByName(search_input);
+        if(product != null){
+            ctx.json(product);
         }
     }
     private void getFiltersHandler(Context ctx) throws JsonProcessingException{
         String filters = ctx.pathParam("filters");
-        ctx.json(groceryService.getGroceriesByFilters(filters));
+        ctx.json(productService.getGroceriesByFilters(filters));
     }
 }
