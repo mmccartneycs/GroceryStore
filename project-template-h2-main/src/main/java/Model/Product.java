3cf8b50e06507adbfae57442c79c155e54da3a09
mmccartneycs <102608673+mmccartneycs@users.noreply.github.com>
2023-03-07 13:18:30 -0500

    models

---
+++ project-template-h2-main/src/main/java/Model/Product.java
@@ -0,0 +1,29 @@
+public class Product{
+    public int productId;
+    public String userEmail;
+    public String password;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ project-template-h2-main/src/main/java/Model/User.java
@@ -0,0 +1,30 @@
+public class User {
+    public int userId;
+    public String userEmail;
+    public String password;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
